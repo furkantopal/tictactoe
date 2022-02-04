fd8bbbdfce1f7f00035f5c5733998c42d756f523
Furkan Topal <topallfurkan@gmail.com>
2022-02-04 16:48:49 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/tictactoe.ts
+++ src/main/tictactoe.ts
@@ -1,7 +1,16 @@
 export class TicTacToe {
-  play(gameMoves: Move[]) {
-    const firstMove: string = gameMoves[0].sign;
-    return firstMove === "X";
+  play(gameMoves: Move[]): string {
+    if (gameMoves[0].sign !== "X") {
+      return "Invalid starter!";
+    }
+
+    for (let i = 0; i < gameMoves.length - 1; i++) {
+      if (gameMoves[i].sign === gameMoves[i + 1].sign) {
+        return "Invalid move based on previous sign!";
+      }
+    }
+
+    return "X";
   }
 }
 
--- src/test/tictactoe.test.ts
+++ src/test/tictactoe.test.ts
@@ -1,21 +1,32 @@
 import { Move, TicTacToe } from "../main/tictactoe";
 
-let gameMoves: Move[] = [
+let invalidStartingGameMove: Move[] = [{ sign: "0", placement: "BL" }];
+
+let invalidMoveBasedOnPreviousSign: Move[] = [
   { sign: "X", placement: "BL" },
-  { sign: "O", placement: "BC" },
+  { sign: "X", placement: "ML" },
+  { sign: "X", placement: "BC" },
+  { sign: "0", placement: "BC" },
   { sign: "X", placement: "BR" },
-  { sign: "O", placement: "ML" },
 ];
 
 describe("TicTacToe test", () => {
   let ticTacToe: TicTacToe = new TicTacToe();
 
-  it("should be that first placement is X", () => {
-    expect(ticTacToe.play(gameMoves)).toBeTruthy();
+  it("should start with X", () => {
+    expect(ticTacToe.play(invalidStartingGameMove)).toEqual("Invalid starter!");
   });
 
   it("should be the current move is not the same with the previous move", () => {
-    expect(ticTacToe.play(gameMoves)).toBeTruthy();
+    expect(ticTacToe.play(invalidMoveBasedOnPreviousSign)).toEqual(
+      "Invalid move based on previous sign!"
+    );
+  });
+
+  it("should be on a new placement", () => {
+    expect(ticTacToe.play(invalidMoveBasedOnPreviousSign)).toEqual(
+      "Invalid placement!"
+    );
   });
 });
 
